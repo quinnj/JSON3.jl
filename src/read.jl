ce5e08c9b65ffca01a0479aae734a26de25b2492
quinnj <quinn.jacobd@gmail.com>
2019-05-14 23:18:10 -0600

    More work on struct integration

---
+++ src/read.jl
@@ -0,0 +1,541 @@
+function read(str::String)
+    buf = codeunits(str)
+    len = length(buf)
+    if len == 0
+        error = UnexpectedEOF
+        pos = 0
+        @goto invalid
+    end
+    pos = 1
+    @inbounds b = buf[pos]
+    @wh
+    tape = len > div(Mmap.PAGESIZE, 2) ? Mmap.mmap(Vector{UInt64}, len) :
+        Vector{UInt64}(undef, len + 2)
+    pos, tapeidx = read!(buf, 1, len, b, tape, 1, Any)
+    @inbounds t = tape[1]
+    if isobject(t)
+        return Object(buf, tape)
+    elseif isarray(t)
+        return Array{geteltype(tape[2])}(buf, tape)
+    else
+        return getvalue(Any, buf, tape, 1, t)
+    end
+@label invalid
+    invalid(error, buf, pos, Any)
+end
+
+function read(str::String, ::Type{T}) where {T}
+    buf = codeunits(str)
+    len = length(buf)
+    if len == 0
+        error = UnexpectedEOF
+        pos = 0
+        @goto invalid
+    end
+    pos = 1
+    @inbounds b = buf[pos]
+    @wh
+    pos, x = read(buf, pos, len, b, T)
+    return x
+@label invalid
+    invalid(error, buf, pos, T)
+end
+
+function read(buf, pos, len, b, U::Union)
+    try
+        return read(buf, pos, len, b, U.a)
+    catch e
+        return read(buf, pos, len, b, U.b)
+    end
+end
+
+function read!(buf::AbstractVector{UInt8}, pos, len, b, tape, tapeidx, ::Type{Any})
+    if b == UInt8('{')
+        return read!(buf, pos, len, b, tape, tapeidx, Object)
+    elseif b == UInt8('[')
+        return read!(buf, pos, len, b, tape, tapeidx, Array)
+    elseif b == UInt8('"')
+        return read!(buf, pos, len, b, tape, tapeidx, String)
+    elseif b == UInt8('n')
+        return read!(buf, pos, len, b, tape, tapeidx, Nothing)
+    elseif b == UInt8('t')
+        return read!(buf, pos, len, b, tape, tapeidx, True)
+    elseif b == UInt8('f')
+        return read!(buf, pos, len, b, tape, tapeidx, False)
+    elseif (UInt8('0') <= b <= UInt8('9')) || b == UInt8('-') || b == UInt8('+')
+        float, code, floatpos = Parsers.typeparser(Float64, buf, pos, len, b, Int16(0), Parsers.OPTIONS)
+        if code > 0
+            int = unsafe_trunc(Int64, float)
+            if int == float
+                @inbounds tape[tapeidx] = INT
+                @inbounds tape[tapeidx + 1] = Core.bitcast(UInt64, int)
+            else
+                @inbounds tape[tapeidx] = FLOAT
+                @inbounds tape[tapeidx + 1] = Core.bitcast(UInt64, float)
+            end
+            return floatpos, tapeidx + 2
+        end
+    end
+@label invalid
+    invalid(InvalidChar, buf, pos, Any)
+end
+
+function read!(buf::AbstractVector{UInt8}, pos, len, b, ::Type{Any})
+    if b == UInt8('{')
+        return read(buf, pos, len, b, NamedTuple)
+    elseif b == UInt8('[')
+        return read(buf, pos, len, b, Base.Array)
+    elseif b == UInt8('"')
+        return read(buf, pos, len, b, String)
+    elseif b == UInt8('n')
+        return read(buf, pos, len, b, Nothing)
+    elseif b == UInt8('t')
+        return read(buf, pos, len, b, Bool)
+    elseif b == UInt8('f')
+        return read(buf, pos, len, b, Bool)
+    elseif (UInt8('0') <= b <= UInt8('9')) || b == UInt8('-') || b == UInt8('+')
+        float, code, pos = Parsers.typeparser(Float64, buf, pos, len, b, Int16(0), Parsers.OPTIONS)
+        if code > 0
+            int = unsafe_trunc(Int64, float)
+            return pos, ifelse(int == float, int, float)
+        end
+    end
+@label invalid
+    invalid(InvalidChar, buf, pos, Any)
+end
+
+function read!(buf::AbstractVector{UInt8}, pos, len, b, tape, tapeidx, ::Type{String})
+    if b != UInt8('"')
+        error = ExpectedOpeningQuoteChar
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    strpos = pos
+    strlen = 0
+    escaped = false
+    @inbounds b = buf[pos]
+    # positioned at first character of object key
+    while b != UInt8('"')
+        if b == UInt8('\\')
+            escaped = true
+            # skip next character
+            pos += 2
+            strlen += 2
+        else
+            pos += 1
+            strlen += 1
+        end
+        @eof
+        @inbounds b = buf[pos]
+    end
+    @inbounds tape[tapeidx] = string(strlen)
+    @inbounds tape[tapeidx+1] = ifelse(escaped, ESCAPE_BIT | strpos, strpos)
+    return pos + 1, tapeidx + 2
+
+@label invalid
+    invalid(error, buf, pos, String)
+end
+
+function read!(buf::AbstractVector{UInt8}, pos, len, b, ::Type{String})
+    if b != UInt8('"')
+        error = ExpectedOpeningQuoteChar
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    strpos = pos
+    strlen = 0
+    escaped = false
+    @inbounds b = buf[pos]
+    # positioned at first character of object key
+    while b != UInt8('"')
+        if b == UInt8('\\')
+            escaped = true
+            # skip next character
+            pos += 2
+            strlen += 2
+        else
+            pos += 1
+            strlen += 1
+        end
+        @eof
+        @inbounds b = buf[pos]
+    end
+    ptr = pointer(buf, strpos)
+    return pos + 1, escaped ? unescape(PointerString(ptr, strlen)) : unsafe_string(ptr, strlen)
+
+@label invalid
+    invalid(error, buf, pos, String)
+end
+
+function read!(buf::AbstractVector{UInt8}, pos, len, b, ::Type{Char})
+    if b != UInt8('"')
+        error = ExpectedOpeningQuoteChar
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    @inbounds x = Char(buf[pos])
+    pos += 1
+    @eof
+    @inbounds b = buf[pos]
+    if b != UInt8('"')
+        error = InvalidChar
+        @goto invalid
+    end
+    return pos + 1, x
+
+@label invalid
+    invalid(error, buf, pos, Char)
+end
+
+struct True end
+function read!(buf::AbstractVector{UInt8}, pos, len, b, tape, tapeidx, ::Type{True})
+    if pos + 3 <= len &&
+        b            == UInt8('t') &&
+        buf[pos + 1] == UInt8('r') &&
+        buf[pos + 2] == UInt8('u') &&
+        buf[pos + 3] == UInt8('e')
+        @inbounds tape[tapeidx] = BOOL | UInt64(1)
+        return pos + 4, tapeidx + 2
+    else
+        invalid(InvalidChar, buf, pos, True)
+    end
+end
+
+struct False end
+function read!(buf::AbstractVector{UInt8}, pos, len, b, tape, tapeidx, ::Type{False})
+    if pos + 4 <= len &&
+        b            == UInt8('f') &&
+        buf[pos + 1] == UInt8('a') &&
+        buf[pos + 2] == UInt8('l') &&
+        buf[pos + 3] == UInt8('s') &&
+        buf[pos + 4] == UInt8('e')
+        @inbounds tape[tapeidx] = BOOL
+        return pos + 5, tapeidx + 2
+    else
+        invalid(InvalidChar, buf, pos, False)
+    end
+end
+
+function read!(buf::AbstractVector{UInt8}, pos, len, b, ::Type{Bool})
+    if pos + 3 <= len &&
+        b            == UInt8('t') &&
+        buf[pos + 1] == UInt8('r') &&
+        buf[pos + 2] == UInt8('u') &&
+        buf[pos + 3] == UInt8('e')
+        return pos + 4, true
+    elseif pos + 4 <= len &&
+        b            == UInt8('f') &&
+        buf[pos + 1] == UInt8('a') &&
+        buf[pos + 2] == UInt8('l') &&
+        buf[pos + 3] == UInt8('s') &&
+        buf[pos + 4] == UInt8('e')
+        return pos + 5, false
+    else
+        invalid(InvalidChar, buf, pos, Bool)
+    end
+end
+
+function read!(buf::AbstractVector{UInt8}, pos, len, b, tape, tapeidx, ::Type{Nothing})
+    if pos + 3 <= len &&
+        b            == UInt8('n') &&
+        buf[pos + 1] == UInt8('u') &&
+        buf[pos + 2] == UInt8('l') &&
+        buf[pos + 3] == UInt8('l')
+        @inbounds tape[tapeidx] = NULL
+        return pos + 4, tapeidx + 2
+    else
+        invalid(InvalidChar, buf, pos, Nothing)
+    end
+end
+
+function read!(buf::AbstractVector{UInt8}, pos, len, b, T::Union{Type{Missing}, Type{Nothing}})
+    if pos + 3 <= len &&
+        b            == UInt8('n') &&
+        buf[pos + 1] == UInt8('u') &&
+        buf[pos + 2] == UInt8('l') &&
+        buf[pos + 3] == UInt8('l')
+        return pos + 4, T === Missing ? missing : nothing
+    else
+        invalid(InvalidChar, buf, pos, T)
+    end
+end
+
+function read(buf, pos, len, b, ::Type{T}) where {T <: Integer}
+    int, code, pos = Parsers.typeparser(T, buf, pos, len, b, Int16(0), Parsers.OPTIONS)
+    if code > 0
+        return pos, int
+    end
+    invalid(InvalidChar, buf, pos, T)
+end
+
+function read(buf, pos, len, b, ::Type{T}) where {T <: AbstractFloat}
+    float, code, pos = Parsers.typeparser(T, buf, pos, len, b, Int16(0), Parsers.OPTIONS)
+    if code > 0
+        return pos, float
+    end
+    invalid(InvalidChar, buf, pos, T)
+end
+
+function read!(buf::AbstractVector{UInt8}, pos, len, b, tape, tapeidx, ::Type{Object})
+    objidx = tapeidx
+    eT = EMPTY
+    if b != UInt8('{')
+        error = ExpectedOpeningObjectChar
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    @inbounds b = buf[pos]
+    @wh
+    if b == UInt8('}')
+        @inbounds tape[tapeidx] = object(2)
+        @inbounds tape[tapeidx+1] = eltypelen(eT, 0)
+        tapeidx += 2
+        pos += 1
+        @goto done
+    elseif b != UInt8('"')
+        error = ExpectedOpeningQuoteChar
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    tapeidx += 2
+    nelem = 0
+    while true
+        keypos = pos
+        keylen = 0
+        escaped = false
+        # read first key character
+        @inbounds b = buf[pos]
+        # positioned at first character of object key
+        while b != UInt8('"')
+            if b == UInt8('\\')
+                escaped = true
+                # skip next character
+                pos += 2
+                keylen += 2
+            else
+                pos += 1
+                keylen += 1
+            end
+            @eof
+            @inbounds b = buf[pos]
+        end
+        @inbounds tape[tapeidx] = string(keylen)
+        @inbounds tape[tapeidx+1] = ifelse(escaped, ESCAPE_BIT | keypos, keypos)
+        tapeidx += 2
+        pos += 1
+        @eof
+        @inbounds b = buf[pos]
+        @wh
+        if b != UInt8(':')
+            error = ExpectedSemiColon
+            @goto invalid
+        end
+        pos += 1
+        @eof
+        @inbounds b = buf[pos]
+        @wh
+        # now positioned at start of value
+        prevtapeidx = tapeidx
+        pos, tapeidx = read!(buf, pos, len, b, tape, tapeidx, Any)
+        @eof
+        @inbounds b = buf[pos]
+        @wh
+        @inbounds eT = promoteeltype(eT, gettypemask(tape[prevtapeidx]))
+        nelem += 1
+        if b == UInt8('}')
+            @inbounds tape[objidx] = object(tapeidx - objidx)
+            @inbounds tape[objidx+1] = eltypelen(eT, nelem)
+            pos += 1
+            @goto done
+        elseif b != UInt8(',')
+            error = ExpectedComma
+            @goto invalid
+        end
+        pos += 1
+        @eof
+        @inbounds b = buf[pos]
+        @wh
+        if b != UInt8('"')
+            error = ExpectedOpeningQuoteChar
+            @goto invalid
+        end
+        pos += 1
+        @eof
+    end
+
+@label done
+    return pos, tapeidx
+@label invalid
+    invalid(error, buf, pos, Object)
+end
+
+function read!(buf::AbstractVector{UInt8}, pos, len, b, ::Type{NamedTuple})
+    if b != UInt8('{')
+        error = ExpectedOpeningObjectChar
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    @inbounds b = buf[pos]
+    @wh
+    if b == UInt8('}')
+        return pos + 1, NamedTuple()
+    elseif b != UInt8('"')
+        error = ExpectedOpeningQuoteChar
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    keys = Symbol[]
+    vals = Any[]
+    while true
+        keypos = pos
+        keylen = 0
+        escaped = false
+        # read first key character
+        @inbounds b = buf[pos]
+        # positioned at first character of object key
+        while b != UInt8('"')
+            if b == UInt8('\\')
+                escaped = true
+                # skip next character
+                pos += 2
+                keylen += 2
+            else
+                pos += 1
+                keylen += 1
+            end
+            @eof
+            @inbounds b = buf[pos]
+        end
+        push!(keys, escaped ? Symbol(unescape(PointerString(pointer(buf, keypos), keylen))) : _symbol(pointer(buf, keypos), keylen))
+        pos += 1
+        @eof
+        @inbounds b = buf[pos]
+        @wh
+        if b != UInt8(':')
+            error = ExpectedSemiColon
+            @goto invalid
+        end
+        pos += 1
+        @eof
+        @inbounds b = buf[pos]
+        @wh
+        # now positioned at start of value
+        pos, y = read!(buf, pos, len, b, Any)
+        push!(vals, y)
+        @eof
+        @inbounds b = buf[pos]
+        @wh
+        if b == UInt8('}')
+            return pos + 1, NamedTuple{Tuple(keys)}(Tuple(vals))
+        elseif b != UInt8(',')
+            error = ExpectedComma
+            @goto invalid
+        end
+        pos += 1
+        @eof
+        @inbounds b = buf[pos]
+        @wh
+        if b != UInt8('"')
+            error = ExpectedOpeningQuoteChar
+            @goto invalid
+        end
+        pos += 1
+        @eof
+    end
+
+@label invalid
+    invalid(error, buf, pos, Object)
+end
+
+function read!(buf::AbstractVector{UInt8}, pos, len, b, tape, tapeidx, ::Type{Array})
+    arridx = tapeidx
+    eT = EMPTY
+    if b != UInt8('[')
+        error = ExpectedOpeningArrayChar
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    @inbounds b = buf[pos]
+    @wh
+    if b == UInt8(']')
+        @inbounds tape[tapeidx] = array(2)
+        @inbounds tape[tapeidx+1] = eltypelen(eT, 0)
+        tapeidx += 2
+        pos += 1
+        @goto done
+    end
+    tapeidx += 2
+    nelem = 0
+    while true
+        # positioned at start of value
+        prevtapeidx = tapeidx
+        pos, tapeidx = read!(buf, pos, len, b, tape, tapeidx, Any)
+        @eof
+        @inbounds b = buf[pos]
+        @wh
+        @inbounds eT = promoteeltype(eT, gettypemask(tape[prevtapeidx]))
+        nelem += 1
+        if b == UInt8(']')
+            @inbounds tape[arridx] = array(tapeidx - arridx)
+            @inbounds tape[arridx+1] = eltypelen(eT, nelem)
+            pos += 1
+            @goto done
+        elseif b != UInt8(',')
+            error = ExpectedComma
+            @goto invalid
+        end
+        pos += 1
+        @eof
+        @inbounds b = buf[pos]
+        @wh
+    end
+
+@label done
+    return pos, tapeidx
+@label invalid
+    invalid(error, buf, pos, Array)
+end
+
+function read!(buf::AbstractVector{UInt8}, pos, len, b, ::Type{A}) where {A <: AbstractArray{T}} where {T}
+    if b != UInt8('[')
+        error = ExpectedOpeningArrayChar
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    @inbounds b = buf[pos]
+    @wh
+    vals = A(undef, 0)
+    if b == UInt8(']')
+        return pos + 1, vals
+    end
+    while true
+        # positioned at start of value
+        pos, y = read!(buf, pos, len, b, T)
+        push!(vals, y)
+        @eof
+        @inbounds b = buf[pos]
+        @wh
+        if b == UInt8(']')
+            return pos + 1, vals
+        elseif b != UInt8(',')
+            error = ExpectedComma
+            @goto invalid
+        end
+        pos += 1
+        @eof
+        @inbounds b = buf[pos]
+        @wh
+    end
+
+@label invalid
+    invalid(error, buf, pos, A)
+end
--- src/structs.jl
+++ src/structs.jl
@@ -7,51 +7,44 @@ struct MutableSetField <: StructType end
 struct KeywordArgConstrutor <: StructType end
 StructType(T) = Ordered()
 
-function read(buf, pos, len, b, ::Type{T}) where {T <: Integer}
-    int, code, pos = Parsers.typeparser(Int64, buf, pos, len, b, Int16(0), Parsers.OPTIONS)
-    if code > 0
-        return pos, int
-    end
-    invalid(InvalidChar, buf, pos, T)
-end
-
-function read(str::String, ::Type{T}) where {T}
-    buf = codeunits(str)
-    len = length(buf)
-    error = InvalidChar
-    pos = 1
-    @eof
-    @inbounds b = buf[pos]
-    @wh
+@inline function read(::MutableSetField, buf, pos, len, b, ::Type{T}) where {T}
     if b != UInt8('{')
         error = ExpectedOpeningObjectChar
         @goto invalid
     end
+    x = T()
     pos += 1
     @eof
     @inbounds b = buf[pos]
     @wh
     if b == UInt8('}')
-        x = T()
         pos += 1
-        @goto done
+        return pos, x
     elseif b != UInt8('"')
         error = ExpectedOpeningQuoteChar
         @goto invalid
     end
     pos += 1
     @eof
-    x = T((ntuple(fieldcount(T)) do i
+    while true
+        keypos = pos
+        keylen = 0
+        escaped = false
         @inbounds b = buf[pos]
         while b != UInt8('"')
             if b == UInt8('\\')
+                escaped = true
+                # skip next character
                 pos += 2
+                keylen += 2
             else
                 pos += 1
+                keylen += 1
             end
             @eof
             @inbounds b = buf[pos]
         end
+        key = escaped ? Symbol(unescape(PointerString(pointer(buf, keypos), keylen))) : _symbol(pointer(buf, keypos), keylen)
         pos += 1
         @eof
         @inbounds b = buf[pos]
@@ -65,13 +58,14 @@ function read(str::String, ::Type{T}) where {T}
         @inbounds b = buf[pos]
         @wh
         # read value
-        pos, y = read(buf, pos, len, b, fieldtype(T, i))
+        pos, y = read(buf, pos, len, b, fieldtype(T, key))
+        setfield!(x, key, y)
         @eof
         @inbounds b = buf[pos]
         @wh
         if b == UInt8('}')
             pos += 1
-            return y
+            return pos, x
         elseif b != UInt8(',')
             error = ExpectedComma
             @goto invalid
@@ -86,14 +80,147 @@ function read(str::String, ::Type{T}) where {T}
         end
         pos += 1
         @eof
-        return y
-        @label invalid
-            invalid(error, buf, pos, T)
-    end)...)
+    end
 
-@label done
-    return pos, x
 @label invalid
     invalid(error, buf, pos, T)
 end
 
+@inline function read(::Ordered, buf, pos, len, b, ::Type{T}) where {T}
+    if b != UInt8('{')
+        error = ExpectedOpeningObjectChar
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    @inbounds b = buf[pos]
+    @wh
+    if b == UInt8('}')
+        pos += 1
+        return pos, T()
+    elseif b != UInt8('"')
+        error = ExpectedOpeningQuoteChar
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    pos, x1 = readvalue(buf, pos, len, fieldtype(T, 1))
+    if fieldcount(T) == 1
+        return pos, T(x1)
+    end
+    pos, x2 = readvalue(buf, pos, len, fieldtype(T, 2))
+    if fieldcount(T) == 2
+        return pos, T(x1, x2)
+    end
+    pos, x3 = readvalue(buf, pos, len, fieldtype(T, 3))
+    if fieldcount(T) == 3
+        return pos, T(x1, x2, x3)
+    end
+    pos, x4 = readvalue(buf, pos, len, fieldtype(T, 4))
+    if fieldcount(T) == 4
+        return pos, T(x1, x2, x3, x4)
+    end
+    pos, x5 = readvalue(buf, pos, len, fieldtype(T, 5))
+    if fieldcount(T) == 5
+        return pos, T(x1, x2, x3, x4, x5)
+    end
+    pos, x6 = readvalue(buf, pos, len, fieldtype(T, 6))
+    if fieldcount(T) == 6
+        return pos, T(x1, x2, x3, x4, x5, x6)
+    end
+    pos, x7 = readvalue(buf, pos, len, fieldtype(T, 7))
+    if fieldcount(T) == 7
+        return pos, T(x1, x2, x3, x4, x5, x6, x7)
+    end
+    pos, x8 = readvalue(buf, pos, len, fieldtype(T, 8))
+    if fieldcount(T) == 8
+        return pos, T(x1, x2, x3, x4, x5, x6, x7, x8)
+    end
+    pos, x9 = readvalue(buf, pos, len, fieldtype(T, 9))
+    if fieldcount(T) == 9
+        return pos, T(x1, x2, x3, x4, x5, x6, x7, x8, x9)
+    end
+    pos, x10 = readvalue(buf, pos, len, fieldtype(T, 10))
+    if fieldcount(T) == 10
+        return pos, T(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10)
+    end
+    pos, x11 = readvalue(buf, pos, len, fieldtype(T, 11))
+    if fieldcount(T) == 11
+        return pos, T(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11)
+    end
+    pos, x12 = readvalue(buf, pos, len, fieldtype(T, 12))
+    if fieldcount(T) == 12
+        return pos, T(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12)
+    end
+    pos, x13 = readvalue(buf, pos, len, fieldtype(T, 13))
+    if fieldcount(T) == 13
+        return pos, T(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13)
+    end
+    pos, x14 = readvalue(buf, pos, len, fieldtype(T, 14))
+    if fieldcount(T) == 14
+        return pos, T(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14)
+    end
+    pos, x15 = readvalue(buf, pos, len, fieldtype(T, 15))
+    if fieldcount(T) == 15
+        return pos, T(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15)
+    end
+    pos, x16 = readvalue(buf, pos, len, fieldtype(T, 16))
+    if fieldcount(T) == 16
+        return pos, T(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16)
+    end
+    vals = []
+    for i = 17:fieldcount(T)
+        pos, y = readvalue(buf, pos, len, fieldtype(T, i))
+        push!(vals, y)
+    end
+    return pos, T(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, vals...)
+
+@label invalid
+    invalid(error, buf, pos, T)
+end
+
+@inline function readvalue(buf, pos, len, ::Type{T}) where {T}
+    @inbounds b = buf[pos]
+    while b != UInt8('"')
+        pos += ifelse(b == UInt8('\\'), 2, 1)
+        @eof
+        @inbounds b = buf[pos]
+    end
+    pos += 1
+    @eof
+    @inbounds b = buf[pos]
+    @wh
+    if b != UInt8(':')
+        error = ExpectedSemiColon
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    @inbounds b = buf[pos]
+    @wh
+    # read value
+    pos, y = read(buf, pos, len, b, T)
+    @eof
+    @inbounds b = buf[pos]
+    @wh
+    if b == UInt8('}')
+        pos += 1
+        return pos, y
+    elseif b != UInt8(',')
+        error = ExpectedComma
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    @inbounds b = buf[pos]
+    @wh
+    if b != UInt8('"')
+        error = ExpectedOpeningQuoteChar
+        @goto invalid
+    end
+    pos += 1
+    @eof
+    return pos, y
+@label invalid
+    invalid(error, buf, pos, T)
+end
