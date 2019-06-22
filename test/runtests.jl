e81bb3293c25ca02c01cbfe221ca7a99f122672f
Jacob Quinn <quinn.jacobd@gmail.com>
2019-06-22 02:47:57 -0600

    A little more coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -64,7 +64,9 @@ end
 
 @test_throws ArgumentError JSON3.read("")
 @test JSON3.read("{\"hey\":1}").hey == 1
+show(JSON3.read("{\"hey\":1}"))
 @test JSON3.read("[\"hey\",1]") == ["hey",1]
+show(JSON3.read("[\"hey\",1]"))
 @test JSON3.read("1.0") === 1
 @test JSON3.read("1") === 1
 @test JSON3.read("1.1") === 1.1
