1e4cd648a6fe3bd4e3cac037773b57ff5b04db42
Jacob Quinn <quinn.jacobd@gmail.com>
2019-07-22 06:32:37 -0600

    Remove custom show for array

--- src/show.jl
+++ src/show.jl
@@ -1,5 +1,4 @@
 Base.show(io::IO, j::Object) = _show(io, j)
-Base.show(io::IO, j::Array) = _show(io, j)
 _show(io::IO, x, indent=0, offset=0) = show(io, x)
 
 function _show(io::IO, obj::Object, indent=0, offset=0)
