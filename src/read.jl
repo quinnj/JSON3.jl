760748ca2f33497f3da388735f98f5c36f5aec1e
Jacob Quinn <quinn.jacobd@gmail.com>
2020-02-07 00:21:08 -0700

    Improve Mutable read perf by ensuring we merge NamedTuples and not fallback to Dict merging. Fixes #41

--- src/read.jl
+++ src/read.jl
@@ -2,16 +2,17 @@ struct VectorString{T <: AbstractVector{UInt8}} <: AbstractString
     bytes::T
 end
 
-Base.ncodeunits(s::VectorString) = length(s.bytes)
-Base.codeunit(s::VectorString) = UInt8
-Base.length(s::VectorString) = ncodeunits(s)
-function Base.codeunit(s::VectorString, i::Int)
-    @inbounds b = s.bytes[i]
-    return b
-end
-Base.pointer(v::VectorString) = pointer(v.bytes)
-Base.pointer(v::VectorString, i::Integer) = pointer(v.bytes, i)
-Base.unsafe_convert(::Type{Ptr{UInt8}}, v::VectorString) = pointer(v)
+Base.codeunits(x::VectorString) = x.bytes
+# Base.ncodeunits(s::VectorString) = length(s.bytes)
+# Base.codeunit(s::VectorString) = UInt8
+# Base.length(s::VectorString) = ncodeunits(s)
+# function Base.codeunit(s::VectorString, i::Int)
+#     @inbounds b = s.bytes[i]
+#     return b
+# end
+# Base.pointer(v::VectorString) = pointer(v.bytes)
+# Base.pointer(v::VectorString, i::Integer) = pointer(v.bytes, i)
+# Base.unsafe_convert(::Type{Ptr{UInt8}}, v::VectorString) = pointer(v)
 
 # high-level user API functions
 read(io::IO) = read(Base.read(io, String))
