fad6527c3a9f30e4815adac15a9f33d2d754ea95
Jacob Quinn <quinn.jacobd@gmail.com>
2020-02-15 17:07:46 -0700

    Docs update

---
+++ docs/make.jl
@@ -0,0 +1,17 @@
+using Documenter, JSON3
+
+makedocs(;
+    modules=[JSON3],
+    format=Documenter.HTML(),
+    pages=[
+        "Home" => "index.md",
+    ],
+    repo="https://github.com/quinnj/JSON3.jl/blob/{commit}{path}#L{line}",
+    sitename="JSON3.jl",
+    authors="Jacob Quinn",
+    assets=String[],
+)
+
+deploydocs(;
+    repo="github.com/quinnj/JSON3.jl",
+)
