29a7723082179f11790a156da24de06f459d2c6d
Mary McGrath <m.c.mcgrath13@gmail.com>
2021-06-26 20:15:16 -0400

    fix: call the collapsing unions fn

--- src/gentypes.jl
+++ src/gentypes.jl
@@ -156,6 +156,7 @@ Write an `Expr` or `Vector{Expr}` to file.  Formatted so that it can be used wit
 """
 function write_exprs(expr::Expr, io::IOStream)
     remove_line_numbers!(expr)
+    collapse_unions!(expr)
     collapse_singleton_blocks!(expr)
 
     str = repr(expr)[3:end-1] # removes :( and )
