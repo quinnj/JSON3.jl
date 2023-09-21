e11e47590f4cb4f41c50011c7505b74284d714e7
Oscar Smith <oscardssmith@gmail.com>
2023-09-21 15:18:22 -0400

    add nested getindex to precompile
    The top level getindex is `get(obj::JSON3.Object{Vector{UInt8}, Vector{UInt64}}, key)`, but a nested getindex will be using a subarray so it needs different compilation.  `get(obj::JSON3.Object{Vector{UInt8}, SubArray{UInt64, 1, Vector{UInt64}, Tuple{UnitRange{Int64}}, true}}, key`

--- src/workload.jl
+++ src/workload.jl
@@ -1,13 +1,14 @@
 using PrecompileTools
 
 @compile_workload begin
-    str = """{"a": 1, "b": "hello, world", "c": [1, 2], "d": true, "e": null, "f": 1.92}"""
+    str = """{"a": 1, "b": "hello, world", "c": [1, 2], "d": true, "e": null, "f": 1.92, "g": {"a": {"a" : "b"}}}"""
 
     JSON3.read(IOBuffer(str))
     json = JSON3.read(str)
     for i in "abcdef"
         json[i]
     end
+    json[:g][:a][:a]
 
     JSON3.read(
         str,
