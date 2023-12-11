915505862fbde6cb8d4544f8ddcd317d7b2c3c91
Phillip Alday <phillip.alday@beacon.bio>
2023-12-11 13:18:09 -0600

    1.6 compat

--- test/runtests.jl
+++ test/runtests.jl
@@ -1103,6 +1103,8 @@ y = Vector{UndefGuy}(undef, 2)
 y[1] = x
 @test JSON3.write(y) == "[{\"id\":10},null]"
 
-@testset "Arrow" include("arrow.jl")
+@static if isdefined(Base, :get_extension)
+    @testset "Arrow" include("arrow.jl")
+end
 
 end # @testset "JSON3"
